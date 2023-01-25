66aba040dafffd99e86b1349788e5955264e77b6
erikbernard <erikbrsilva@gmail.com>
2023-01-24 23:39:16 -0300

    feat: high contrast theme implementation and font size control

--- js/theme.js
+++ js/theme.js
@@ -1,21 +1,20 @@
-var toggle = document.getElementById('theme-toggle');
-
-var storedTheme =
-  localStorage.getItem('theme') ||
-  (window.matchMedia('(prefers-color-scheme: dark)').matches
-    ? 'dark'
-    : 'light');
-if (storedTheme)
-  document.documentElement.setAttribute('data-theme', storedTheme);
-
-toggle.onclick = function () {
-  var currentTheme = document.documentElement.getAttribute('data-theme');
-  var targetTheme = 'light';
-
-  if (currentTheme === 'light') {
-    targetTheme = 'dark';
-  }
+export const saveTheme = () => {
+  const storedTheme =
+    localStorage.getItem('theme') ||
+    (window.matchMedia('(prefers-color-scheme: dark)').matches
+      ? 'dark'
+      : 'light');
+  if (storedTheme)
+    document.documentElement.setAttribute('data-theme', storedTheme);
+}
+export const changeTheme = () => {
+  const currentTheme = document.documentElement.getAttribute('data-theme');
+  let targetTheme = 'light';
+  
+  currentTheme === 'light' 
+  ? targetTheme = 'dark' 
+  : targetTheme = 'light';
 
   document.documentElement.setAttribute('data-theme', targetTheme);
   localStorage.setItem('theme', targetTheme);
-};
+}
